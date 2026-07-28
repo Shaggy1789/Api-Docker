33fa85eba525d66547f2ec1f8cc850680ff4b482
Shaggy1789 <chavezrios2005@gmail.com>
2026-07-27 23:05:16 -0600

    fix: Basket API - corregir Scrutor.Decorate, UseExceptionHandler, agregar MapHealthChecks

---
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -0,0 +1,42 @@
+using Basket.Api.Data;
+using Basket.Api.Models;
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(conf =>
+{
+    conf.RegisterServicesFromAssembly(assembly);
+    conf.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    conf.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opt.Schema.For<ShoppingCart>().Identity(x => x.Username);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddProblemDetails();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+app.MapCarter();
+app.MapHealthChecks("/healthz");
+app.Run();
